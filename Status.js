81d8788efafa61517f80f16864ee751eb33e6e41
kachchhi <55308892+kachchhi@users.noreply.github.com>
2019-12-16 07:43:41 +0530

    Update Status.js

--- Status.js
+++ Status.js
@@ -3,7 +3,7 @@ import {StyleSheet, AsyncStorage, BackHandler, View, Image} from 'react-native';
 import {} from 'native-base';
 import image from './img/12345.jpg';
 
-export default class App extends React.Component {
+export default class Status extends React.Component {
   render() {
     return (
       <View style={{height: 50, width: 50}}>
